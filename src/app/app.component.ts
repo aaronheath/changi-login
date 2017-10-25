0d3b6205c70af292cb8b000df1bdadbb2aa21bc7
Aaron Heath <aaron@aaronheath.com>
2017-10-25 16:46:44 +1030

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {FormBuilder} from "@angular/forms";
+import {Http} from "@angular/http";
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  form = this.fb.group({
+    email: '',
+    password: '',
+  });
+
+  constructor(private fb: FormBuilder, private http: Http) {}
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    console.log('submitted', this.form.value);
+
+    this.http
+      .get('http://api.icndb.com/jokes/random')
+      .map(response => response.json())
+      .subscribe(response => console.log(response));
+
+    console.log('here', event);
+  }
 }
